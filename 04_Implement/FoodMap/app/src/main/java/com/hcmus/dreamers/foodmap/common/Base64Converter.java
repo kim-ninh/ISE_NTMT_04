7c028926aef84fba013e040574abe1b0eb231fea
Kim Ninh Huynh <kimninh1610@gmail.com>
2018-11-10 14:34:14 +0700

    use bitmap to hold image file

--- 04_Implement/FoodMap/app/src/main/java/com/hcmus/dreamers/foodmap/common/Base64Converter.java
+++ 04_Implement/FoodMap/app/src/main/java/com/hcmus/dreamers/foodmap/common/Base64Converter.java
@@ -1,9 +1,15 @@
 package com.hcmus.dreamers.foodmap.common;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.util.Base64;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -25,5 +31,18 @@ public class Base64Converter {
         bytes = output.toByteArray();
         String encodedString = Base64.encodeToString(bytes, Base64.DEFAULT);
         return  encodedString;
+    }
+
+    public static String encodeToBase64(Context context, Uri imageUri)throws FileNotFoundException {
+
+        InputStream inputStream;
+        inputStream = context.getContentResolver().openInputStream(imageUri);
+        Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG,100, byteArrayOutputStream);
+        byte[] bytes = byteArrayOutputStream.toByteArray();
+        String encodedImage = Base64.encodeToString(bytes, Base64.DEFAULT);
+        return encodedImage;
     }
 }
