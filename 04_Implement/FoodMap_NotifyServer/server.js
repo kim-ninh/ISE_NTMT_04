d340c2be275a03f8bb686e27744482a32939f330
phuocpr1998 <phuocpr1998@gmail.com>
2018-11-13 23:39:13 +0700

    fix server notify

--- 04_Implement/FoodMap_NotifyServer/server.js
+++ 04_Implement/FoodMap_NotifyServer/server.js
@@ -17,10 +17,12 @@ io.sockets.on('connection', function (socket) {
 	//io.sockets.emit('serverguitinnhan', { noidung: "okbaby" });
   socket.on('register', function (email) {//email of user ---------- send from user when connect
 	if(email){
-			socket.email = email;
-			users.push(socket);
-			console.log("Email: " + socket.email);
-			console.log("ID: " + socket.id);
+			if (users.indexOf(email) == -1){
+				socket.email = email;
+				users.push(socket);
+				console.log("Email: " + socket.email);
+				console.log("ID: " + socket.id);
+			}
 	}
   });
   
@@ -36,10 +38,12 @@ io.sockets.on('connection', function (socket) {
   //}
   
   socket.on('send_order', function(data){ //
+	  console.log('send_order' + data);
 	  var order = JSON.parse(data);
 	  var index = users.findIndex(soc => soc.email === order.email_owner);
 	  if(index != -1){
 		  var id = users[index].id;
+		  console.log('id_send_order' + id);
 		  io.to(id).emit('receive_order', data);
 	  }else{
 		  socket.emit('result', {status:404, message:'Order fail!'});
@@ -56,11 +60,12 @@ io.sockets.on('connection', function (socket) {
   //}
   
   socket.on('send_result', function(response){ //
-		console.log(response);
+	  console.log('send_result' + response);
 	  var resp = JSON.parse(response);
 	  var index = users.findIndex(soc => soc.email === resp.email_guest);
 	  if(index != -1){
 		  var id = users[index].id;
+		  console.log('id_send_result' + id);
 		  io.to(id).emit('receive_result', response);
 	  }else{
 		  socket.emit('result', {status:404, message:'Turn back fail!'});
