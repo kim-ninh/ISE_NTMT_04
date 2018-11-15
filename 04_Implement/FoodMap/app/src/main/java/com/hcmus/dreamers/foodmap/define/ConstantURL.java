dd3256859d2f8fc2f0b18977546863ff7f768bb8
Châu Hoàng Phúc <chauhoangphuc432@gmail.com>
2018-11-15 20:46:01 +0700

    create service

--- 04_Implement/FoodMap/app/src/main/java/com/hcmus/dreamers/foodmap/define/ConstantURL.java
+++ 04_Implement/FoodMap/app/src/main/java/com/hcmus/dreamers/foodmap/define/ConstantURL.java
@@ -36,4 +36,5 @@ public interface ConstantURL {
     public static final String PHOTONAPI                   = "http://photon.komoot.de/api/";
     public static final String PHOTONAPIREVERSE            = "http://photon.komoot.de/reverse";
     public static final String IMAGE_RELATIVE_PATH         = "./images/%d/%s";              // %d: id_rest, %s: imageName
+    public static final String URLWEBSOCKET                = "http://foodmap-notifyserver.herokuapp.com/";
 }
