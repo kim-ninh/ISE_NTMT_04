72caa9c9a7ba78b0faf97794fddb4cd140a3239e
Kim Ninh Huynh <kimninh1610@gmail.com>
2018-11-16 23:00:14 +0700

    pick photo from camera/galery supported in edit restaurant layout

--- 04_Implement/FoodMap/app/src/main/java/com/hcmus/dreamers/foodmap/fragment/RestaurantInfoFragment.java
+++ 04_Implement/FoodMap/app/src/main/java/com/hcmus/dreamers/foodmap/fragment/RestaurantInfoFragment.java
@@ -5,7 +5,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.FileProvider;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -15,6 +19,7 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
@@ -23,11 +28,15 @@ import com.google.gson.Gson;
 import com.hcmus.dreamers.foodmap.AsyncTask.DownloadImageTask;
 import com.hcmus.dreamers.foodmap.AsyncTask.TaskCompleteCallBack;
 import com.hcmus.dreamers.foodmap.EditRestaurantActivity;
+import com.hcmus.dreamers.foodmap.ManageAccountActivity;
 import com.hcmus.dreamers.foodmap.Model.Restaurant;
 import com.hcmus.dreamers.foodmap.R;
 import com.hcmus.dreamers.foodmap.common.FoodMapApiManager;
 import com.hcmus.dreamers.foodmap.define.ConstantCODE;
+import com.theartofdev.edmodo.cropper.CropImage;
 
+import java.io.File;
+import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -49,6 +58,11 @@ public class RestaurantInfoFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+
+    private static final int REQUEST_OPEN_GALERY = 12345;
+    private static final int REQUEST_TAKE_PHOTO = 11111;
+
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -176,6 +190,42 @@ public class RestaurantInfoFragment extends Fragment {
                 mTimePicker.show();
             }
         });
+
+        imgDescription.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Đổi hình đại diện (Từ máy ảnh hoặc thư viện)
+
+                PopupMenu popupMenu = new PopupMenu(context, view);
+                // Inflating the popup using xml file
+                popupMenu.getMenuInflater().inflate(R.menu.pick_image_menu, popupMenu.getMenu());
+                // registering popup with OnMenuItemClickListener
+
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+
+                        int itemID = item.getItemId();
+                        switch (itemID){
+                            case R.id.open_camera:
+                                dispatchTakePictureIntent();
+                                break;
+
+
+                            case R.id.open_galery:
+
+                                Intent intent = new Intent(Intent.ACTION_PICK,
+                                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                                startActivityForResult(intent, REQUEST_OPEN_GALERY);
+
+                                break;
+                        }
+                        return true;
+                    }
+                });
+                popupMenu.show();
+            }
+        });
     }
 
     private void putDataToViews() {
@@ -263,6 +313,7 @@ public class RestaurantInfoFragment extends Fragment {
             restaurant.setAddress(txtAddress.getText().toString());
             restaurant.setTimeOpen(openHour);
             restaurant.setTimeClose(closeHour);
+            restaurant.setDescription(txtDescription.getText().toString());
 
             return true;
         }
@@ -327,5 +378,81 @@ public class RestaurantInfoFragment extends Fragment {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    String mCurrentPhotoPath;
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+    Uri photoURI;
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(context.getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                Log.e("CreateImageFile", ex.getMessage());
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                photoURI = FileProvider.getUriForFile(context,
+                        "com.hcmus.dreamers.foodmap",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+
+            startCropImageActivity(photoURI);
+        }
+
+        if (requestCode == REQUEST_OPEN_GALERY && resultCode == RESULT_OK)
+        {
+            // Chuỗi URI trả về có dạng content://<path>
+            Uri imageUri = Uri.parse(data.getDataString());
+            String str = imageUri.getPath();
+            startCropImageActivity(imageUri);
+        }
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                Uri resultUri = result.getUri();
+
+                imgDescription.setImageURI(resultUri);
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+                Toast.makeText(context,
+                        error.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private void startCropImageActivity(Uri imageUri) {
+        CropImage.activity(imageUri)
+                .start(getContext(),this);
     }
 }
